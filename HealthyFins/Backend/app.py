84d4493ba1550e7c7785d25c9c601e3dd46e0893
Ajay Raj <ajayraj30002@gmail.com>
2026-05-22 12:56:46 +0530

    Update app.py

--- HealthyFins/Backend/app.py
+++ HealthyFins/Backend/app.py
@@ -42,11 +42,8 @@ app = FastAPI(
 
 # ========== CORS CONFIGURATION ==========
 origins = [
-    "https://healthy-fins.vercel.app",
-    "http://localhost:3000",
-    "http://localhost:5500",
-    "http://127.0.0.1:5500",
-    "*"
+    "https://healthy-fins.vercel.app"
+    
 ]
 
 app.add_middleware(
